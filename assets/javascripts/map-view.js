ee6e038835805fd3843978f99c36745db610af84
Gregory Grillone <greg.grillone@gmail.com>
2012-06-03 10:42:06 -0400

    Finish basic map view of baby clothing category

--- assets/javascripts/map-view.js
+++ assets/javascripts/map-view.js
@@ -1,15 +1,47 @@
 $(document).ready(function() {
-	
-	var initialize = function() {
+	//http://www.davidjwatts.com/Simple-jQuery-Mobile-Site-with-Google-Maps-API-V3
+	/*var center;
+	var map = null;
 
+	var initialize = function(lat,lng) {
+
+		center = new google.maps.LatLng(lat,lng);
 		var myOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map-view"),
-            myOptions);
-
+			zoom: 14,
+			center: center,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};		
+		map = new google.maps.Map(document.getElementById("map-view"), myOptions);
 	};
-	//initialize();
+
+	var detectBrowser = function() {
+
+		var useragent = navigator.userAgent;
+		var mapdivMap = $("#map-view");
+
+		if(useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+			mapdivMap.css("width", "100%");
+			mapdivMap.css("height", "100%");
+		}
+		else {
+			mapdivMap.css("width","600px");
+			mapdivMap.css("height","800px");
+		}
+	};
+
+	$("#go-map-view").click(function() {
+
+		if(navigator.geolocation) {
+			detectBrowser();
+			navigator.geolocation.getCurrentPosition(function(position) {
+				initialize(position.coords.latitude, position.coords.longitude);
+			});
+		}
+		else {
+			detectBrowser();
+			initialize(52.636161,-1.133065);
+		}
+	});
+	*/
+
 });
